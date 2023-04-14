3a2637710000217b001c5ef47141af1d4e7ca17c
gogolashvili <ggogolashvili91@gmail.com>
2023-04-14 13:31:10 +0400

    add button new game  cpu

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const choiceButtons = document.querySelectorAll(".btn-box");
 
+let player1 = "x";
+
 const activateChoice = (icon) => {
   if (icon === "x") {
     choiceButtons[0].classList.add("active");
     choiceButtons[1].classList.remove("active");
+    player1 = "x";
   } else {
     choiceButtons[1].classList.add("active");
     choiceButtons[0].classList.remove("active");
+    player1 = "o";
   }
 };
