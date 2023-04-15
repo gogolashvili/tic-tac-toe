40741565f6b6d8d3c3513526722295c5abc350e6
gogolashvili <ggogolashvili91@gmail.com>
2023-04-15 15:18:47 +0400

    add winner Combinations massive

--- main.js
+++ main.js
@@ -8,6 +8,16 @@ let turn = "x";
 let freeButtons = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 let xArray = [];
 let oArray = [];
+let winnerCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 const activateChoice = (icon) => {
   if (icon === "x") {
